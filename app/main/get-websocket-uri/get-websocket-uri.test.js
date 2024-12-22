e5b25185bf8969b810bf0fa3873974692bf10f25
dawid <dawidkapciak@gmail.com>
2024-12-22 15:08:07 +0100

    poe 2 update

--- app/main/get-websocket-uri/get-websocket-uri.test.js
+++ app/main/get-websocket-uri/get-websocket-uri.test.js
@@ -2,12 +2,12 @@ import getWebSocketUri from "./get-websocket-uri";
 
 describe("getWebSocketUri", () => {
   const expectedUrl =
-    "wss://www.pathofexile.com/api/trade/live/Incursion/NK6Ec5";
+    "wss://www.pathofexile.com/api/trade2/live/poe2/Standard/qqDMeRLCg";
 
   describe("when `live` is included in the URL", () => {
     describe("when the last element is a `/`", () => {
       const urlWithSlash =
-        "https://www.pathofexile.com/trade/search/Incursion/NK6Ec5/live/";
+        "https://www.pathofexile.com/trade2/search/poe2/Standard/qqDMeRLCg/live/";
 
       it("returns the formatted WebSocket URL", () => {
         const actualUrl = getWebSocketUri(urlWithSlash);
@@ -18,7 +18,7 @@ describe("getWebSocketUri", () => {
 
     describe("when the last element is not a `/`", () => {
       const urlWithoutSlash =
-        "https://www.pathofexile.com/trade/search/Incursion/NK6Ec5/live";
+        "https://www.pathofexile.com/trade2/search/poe2/Standard/qqDMeRLCg/live";
 
       it("returns the formatted WebSocket URL", () => {
         const actualUrl = getWebSocketUri(urlWithoutSlash);
@@ -31,7 +31,7 @@ describe("getWebSocketUri", () => {
   describe("when `live` is not included in the URL", () => {
     describe("when the last element is a `/`", () => {
       const urlWithSlash =
-        "https://www.pathofexile.com/trade/search/Incursion/NK6Ec5/";
+        "https://www.pathofexile.com/trade2/search/poe2/Standard/qqDMeRLCg/";
 
       it("returns the formatted WebSocket URL", () => {
         const actualUrl = getWebSocketUri(urlWithSlash);
@@ -42,7 +42,7 @@ describe("getWebSocketUri", () => {
 
     describe("when the last element is not a `/`", () => {
       const urlWithoutSlash =
-        "https://www.pathofexile.com/trade/search/Incursion/NK6Ec5";
+        "https://www.pathofexile.com/trade2/search/poe2/Standard/qqDMeRLCg";
 
       it("returns the formatted WebSocket URL", () => {
         const actualUrl = getWebSocketUri(urlWithoutSlash);
